4bdaf1ab239be3e615e43b71d69754215ef4a3d6
Sohan <44776605+Sohanur-Rahman642@users.noreply.github.com>
2025-07-19 18:46:45 +0600

    update documentsDirectory for datastore in ios

--- composeApp/src/iosMain/kotlin/com/aiapp/flowcent/di/Modules.ios.kt
+++ composeApp/src/iosMain/kotlin/com/aiapp/flowcent/di/Modules.ios.kt
@@ -5,11 +5,29 @@
 package com.aiapp.flowcent.di
 
 import com.aiapp.flowcent.core.datastore.createDataStore
+import com.aiapp.flowcent.core.datastore.dataStoreFileName
 import com.aiapp.flowcent.core.platform.FlowCentAi
 import org.koin.core.module.Module
 import org.koin.dsl.module
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSSearchPathForDirectoriesInDomains
+import platform.Foundation.NSUserDomainMask
 
 actual val platformModule: Module = module {
     single { FlowCentAi() }
-    single { createDataStore(get()) }
+    single {
+        createDataStore(
+            producePath = {
+                // Get the standard documents directory path for iOS
+                val documentsDirectory = NSSearchPathForDirectoriesInDomains(
+                    directory = NSDocumentDirectory,
+                    domainMask = NSUserDomainMask,
+                    expandTilde = true
+                ).first() as String
+
+                // Append the file name to create the full path
+                "$documentsDirectory/$dataStoreFileName"
+            }
+        )
+    }
 }
